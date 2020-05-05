221aa837b0e7d9f904404440069a7fdef08336e9
ajisjamsu <aji.sjamsu@gmail.com>
2020-05-05 11:23:30 -0400

    Add offset to chip generator.

--- generate_chips.m
+++ generate_chips.m
@@ -1,13 +1,16 @@
 function [baseband, mod] = generate_chips(data_vec, bit_count, offset)
 %% gen_chips: create chips at baseband
 %{
-Inputs: delay in chips/quarter-chips
+Inputs: data vector, number of data bits, correlation peak offset in samps
 Outputs: mod = modulated BPSK samples, baseband = baseband chip samples
 
     1) Create data bits (50 bps)
     2) Call on CA code generator to make 1ms spreading code
     3) Spread data with spreading code (1023 chips/bit)
 %}
+if nargin<3
+  offset = 0;
+end
 
 %% 1) Create data bits
 load loadconst.mat
@@ -17,7 +20,12 @@ load loadconst.mat
 % Create 1 ms of spreading code upsampled to the proper rate
 goldcode_1ms = cacode(PRN, SAMPS_PER_CHIP);
 
-% Account for code phase offset
+%% 2A) Account for code phase offset
+% Generate code phase offset with xcorr(input_sig, reference_cacode)
+% Resulting correlation offset:
+% If offset = POSITIVE, shift gold code RIGHT by [offset] samples
+% If offset = NEGATIVE, shift gold code LEFT
+goldcode_1ms = circshift(goldcode_1ms, offset);
 
 %% 3) Assemble data vector ms-by-ms, inverting for 0 data bit
 ca_vec = [];
